190627589fa14c849e617c552ada32771ecf4568
tectupobllluk <102186747+tectupobllluk@users.noreply.github.com>
2023-05-05 02:33:45 +0300

    Update StepTracker.java

--- Aplication1/src/StepTracker.java
+++ Aplication1/src/StepTracker.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class StepTracker {
 
     Converter converter = new Converter();
-    MonthData[] monthToData = new MonthData[12];
+    MonthData[] monthToData = new MonthData[12];//инициализацию необходимо перенести в конструктор, чтобы не создавались объекты без вызова конструктора
     Scanner scan;
     int goalByStepsPerDay;
-    int steps;
+    int steps;//Зачем эта переменная, она ни чем не заполняется по ходу программы и передается в методы
 
     StepTracker(Scanner scan) {
 
@@ -20,7 +20,7 @@ public class StepTracker {
 
     void changeStepGoal() {
         System.out.println("Введите новую цель шагов :");
-        int newGoal = scan.nextInt();
+        int newGoal = scan.nextInt();//лучше сделать do-while, чтобы добиться правильного введения цели
         if (newGoal <= 0) {
             System.out.println("Не удалось поставить новую цель, цель = " + goalByStepsPerDay);
         } else {
@@ -32,22 +32,22 @@ public class StepTracker {
 
     void addNewNumbersStepsPerDay() {
 
-        System.out.println("Введите месяц");
-        int month = scan.nextInt();
-        if (month >= 1 && month <= 12) {
-        } else {
+        System.out.println("Введите месяц");//считывание месяца и дня лучше вынести в отдельные методы, 
+        int month = scan.nextInt();         //так повысится читаемость и не нарушится область отвественности метода
+        if (month >= 1 && month <= 12) {//по заданию, номер вводимого месяца должен быть от 0 до 11 включительно (см. шаг 7)
+        } else {                        //В этом задании показывается, что нумерация в массивах начинается с 0
             System.out.println("Месяц введен неверно");
             return;
         }
         System.out.println("Введите день");
-        int days = scan.nextInt();
-        if (days >= 1 && days <= 30) {
+        int days = scan.nextInt();//необходимо добиться введения правильного дня, можно использовать do-while
+        if (days >= 1 && days <= 30) {//почему блок if пустой, поправить
         } else {
             System.out.println("День введен неверно");
             return;
         }
-        System.out.println("Введите количесвто шагов");
-        int steps = scan.nextInt();
+        System.out.println("Введите количесвто шагов");//то же, что и выше
+        int steps = scan.nextInt();//переменная названа также, как глобальная
         if (steps >= 0) {
             monthToData[month - 1].days[days - 1] = steps;
             System.out.println(monthToData[month - 1].days[days - 1]);
@@ -72,18 +72,18 @@ public class StepTracker {
 
             System.out.println("Максимальное количество шагов за месяц : ");
 
-            monthToData[numberMonth-1].maxSteps();
+            monthToData[numberMonth-1].maxSteps();//не выводится
 
             System.out.println("Общее количество шагов за месяц : " +  monthToData[numberMonth-1].sumStepsFromMonth());
 
             System.out.println("Количество сожженых каллорий : ");
 
-            converter.convertStepsToKilokalories(steps);
+            converter.convertStepsToKilokalories(steps);//не выводится
 
-            System.out.println("Вы прошли " + converter.convertStepsToKM(steps) + " киллометров ");
+            System.out.println("Вы прошли " + converter.convertStepsToKM(steps) + " киллометров ");//передается steps, который ничего не содержит
 
             System.out.println("Лучшая серия : " + monthToData[numberMonth-1].bestSeries(goalByStepsPerDay));
-
+//нет вывода среднего количества шагов
 
 
         } else {
